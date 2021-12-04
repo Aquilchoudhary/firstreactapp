4cec0cd68b1ade3186e68cf8cb978c9f954f7578
Aquilchoudhary <90390008+Aquilchoudhary@users.noreply.github.com>
2021-12-04 13:59:21 +0530

    final done

--- src/App.js
+++ src/App.js
@@ -5,23 +5,29 @@ var title = "Calculator";
 var color = "#ffb6c1";
 
 export default function App() {
-  const [a, seta] = useState(0);
-  const [b, setb] = useState(0);
+  const [a, seta] = useState("");
+  const [b, setb] = useState("");
   const [plus, setPlus] = useState(0);
 
-  const inputOne = (event) => seta(event.target.value);
-  const inputTwo = (event) => setb(event.target.value);
-  console.log(seta);
-  console.log(setb);
-  function add(seta, setb) {
-    let c = seta + setb;
-    console.log(c);
-    setPlus(c);
-
-    console.log(seta);
-    console.log(setb);
+  function inputOne(event) {
+    seta(event.target.value);
+  }
+  function inputTwo(event) {
+    setb(event.target.value);
   }
 
+  function add() {
+    setPlus(Number(a) + Number(b));
+  }
+  function subtract() {
+    setPlus(Number(a) - Number(b));
+  }
+  function product() {
+    setPlus(Number(a) * Number(b));
+  }
+  function devide() {
+    setPlus(Number(a) / Number(b));
+  }
   return (
     <div className="App">
       <h1 style={{ backgroundColor: color }}>{title}</h1>
@@ -31,9 +37,11 @@ export default function App() {
       <input type="number" onChange={inputOne} value={a}></input>
       <input type="number" onChange={inputTwo} value={b}></input>
       <button onClick={add}>+</button>
+      <button onClick={subtract}>-</button>
+      <button onClick={product}>*</button>
+      <button onClick={devide}>/</button>
+
       <h1>{plus}</h1>
-      <p>{seta}</p>
-      <p>{setb}</p>
     </div>
   );
 }
